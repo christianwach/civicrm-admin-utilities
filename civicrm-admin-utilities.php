85b39026a50994bd69e8b71445c75b85e4b65053
Christian Wach <needle@haystack.co.uk>
2023-02-23 13:24:09 +0000

    Bump version to 0.9.4

--- civicrm-admin-utilities.php
+++ civicrm-admin-utilities.php
@@ -5,7 +5,7 @@
  * GitHub Plugin URI: https://github.com/christianwach/civicrm-admin-utilities
  * Description: Optionally modifies CiviCRM's behaviour and appearance in single site and multisite installs.
  * Author: Christian Wach
- * Version: 0.9.4a
+ * Version: 0.9.4
  * Author URI: http://haystack.co.uk
  * Text Domain: civicrm-admin-utilities
  * Domain Path: /languages
@@ -18,7 +18,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Set our version here.
-define( 'CIVICRM_ADMIN_UTILITIES_VERSION', '0.9.4a' );
+define( 'CIVICRM_ADMIN_UTILITIES_VERSION', '0.9.4' );
 
 // Trigger logging of 'civicrm_pre' and 'civicrm_post'.
 if ( ! defined( 'CIVICRM_ADMIN_UTILITIES_DEBUG' ) ) {
