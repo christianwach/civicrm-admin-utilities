176f00149e03eef298e0d63d3a48ecfb6e212818
Christian Wach <needle@haystack.co.uk>
2025-07-11 11:46:12 +0100

    Remove confusing user counts

--- assets/templates/site-users-table-views.php
+++ assets/templates/site-users-table-views.php
@@ -20,54 +20,21 @@ defined( 'ABSPATH' ) || exit;
 	<li class="all">
 		<?php $anchor_class = ( ( 'all' === $this->view ) ? 'current' : '' ); ?>
 		<a href="<?php echo esc_url( $url_base ); ?>" class="<?php echo esc_attr( $anchor_class ); ?>">
-			<?php
-
-			printf(
-				/* translators: %s is the placeholder for the count html tag `<span class="count"/>` */
-				esc_html__( 'All %s', 'civicrm-admin-utilities' ),
-				sprintf(
-					'<span class="count">(%s)</span>',
-					esc_html( number_format_i18n( $this->user_counts['all'] ) )
-				)
-			);
-
-			?>
+			<?php esc_html_e( 'All', 'civicrm-admin-utilities' ); ?>
 		</a> |
 	</li>
 
 	<li class="in_civicrm">
 		<?php $anchor_class = ( ( 'in_civicrm' === $this->view ) ? 'current' : '' ); ?>
 		<a href="<?php echo esc_url( add_query_arg( 'user_status', 'in_civicrm', $url_base ) ); ?>" class="<?php echo esc_attr( $anchor_class ); ?>">
-			<?php
-
-			printf(
-				/* translators: %s is the placeholder for the count html `<span class="count"/>` */
-				_n( 'In CiviCRM %s', 'In CiviCRM %s', $this->user_counts['in_civicrm'], 'civicrm-admin-utilities' ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-				sprintf(
-					'<span class="count">(%s)</span>',
-					esc_html( number_format_i18n( $this->user_counts['in_civicrm'] ) )
-				)
-			);
-
-			?>
+			<?php esc_html_e( 'In CiviCRM', 'civicrm-admin-utilities' ); ?>
 		</a> |
 	</li>
 
 	<li class="not_in_civicrm">
 		<?php $anchor_class = ( ( 'not_in_civicrm' === $this->view ) ? 'current' : '' ); ?>
 		<a href="<?php echo esc_url( add_query_arg( 'user_status', 'not_in_civicrm', $url_base ) ); ?>" class="<?php echo esc_attr( $anchor_class ); ?>">
-			<?php
-
-			printf(
-				/* translators: %s is the placeholder for the count html `<span class="count"/>` */
-				_n( 'Not in CiviCRM %s', 'Not in CiviCRM %s', $this->user_counts['not_in_civicrm'], 'civicrm-admin-utilities' ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-				sprintf(
-					'<span class="count">(%s)</span>',
-					esc_html( number_format_i18n( $this->user_counts['not_in_civicrm'] ) )
-				)
-			);
-
-			?>
+			<?php esc_html_e( 'Not in CiviCRM', 'civicrm-admin-utilities' ); ?>
 		</a>
 	</li>
 
